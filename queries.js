1f4046d5be9ffcae70c99c2df05e36a9cb6c20ba
Mbulelo Pani <mbulelopani09@gmail.com>
2021-08-13 11:30:41 +0200

    routes

--- queries.js
+++ queries.js
@@ -4,5 +4,15 @@ const database = require('knex')(connection)
 module.exports = {
     getAll(){
          return database('contacts')
+    },
+    
+    addContact(database, newcontact){
+        return database('contacts')
+        .insert(newcontact)
+        .into("contacts")
+        .returning("*")
+        .then(rows => {
+            return rows[0]
+        })
     }
 }
--- server.js
+++ server.js
@@ -11,10 +11,18 @@ app.use('/', serveStatic(path.join(__dirname, '/dist')))
 app.get('/hh', (request, response) => {
     response.send('We Did it')
 })
-app.get('/b', (request, response) => {
-    queries.getAll().then(results => response.send(results))
+app.get('/contacts', (request, response) => {
+    queries.getAll().then(data => response.send(data))
 })
 
+router.route('/user')                                   // set up user route
+    .post(function(req, res) {                          // listen for a post on user
+        console.log(' -Post -');                        // report a post
+        knex('user').insert({email: req.body.email}).then( function (result) {
+            res.json({ success: true, message: 'ok' });     // respond back to request
+         })   // respond back to request
+    });
+app.use('/api', router); 
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
